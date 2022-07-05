6a6f213420170eef76679e596914e21affd7e4ed
panicdragon <d.takayama.web@gmail.com>
2022-07-06 00:49:33 +0900

    feat: 🎸 create cpu

---
+++ src/cpu.rs
@@ -0,0 +1,33 @@
+use mmu::MMU;
+
+pub struct CPU {
+    pub mmu: MMU,
+    pc: u16,
+    sp: u16,
+    a: u8,
+    f: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    h: u8,
+    l: u8,
+}
+
+impl CPU {
+    pub fn new(rom_name: &str) -> Self {
+        CPU {
+            mmu: MMU::new(rom_name),
+            pc: 0x100,
+            sp: 0,
+            a: 0,
+            f: 0,
+            b: 0,
+            c: 0,
+            d: 0,
+            e: 0,
+            h: 0,
+            l: 0,
+        }
+    }
+}
