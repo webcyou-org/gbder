a6e2f2669c6cdbedbe7c9321cee5cc2a10cd1c1c
panicdragon <d.takayama.web@gmail.com>
2022-07-06 23:05:16 +0900

    refactor: 💡 mod cpu

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use std::env;
 mod cartridge;
 mod bus;
 mod mmu;
+mod cpu;
 
-use mmu::MMU;
+// use mmu::MMU;
+use cpu::CPU;
 
 fn rom_fname() -> String {
     env::args().nth(1).unwrap()
@@ -18,18 +20,19 @@ fn main() {
     // println!("{}", path_buf.to_str().unwrap().to_string());
 
     // let cartridge = Cartridge::new(&rom_fname());
-    let mmu: MMU = MMU::new(&rom_fname());
+    // let mmu: MMU = MMU::new(&rom_fname());
+    let cpu: CPU = CPU::new(&rom_fname());
 
-    println!("{}", mmu.cartridge.title_to_string());
-    println!("{}", mmu.cartridge.rom_to_string());
-    println!("{}", mmu.cartridge.ram_to_string());
-    println!("{:?}", mmu.cartridge.destination_code);
-    println!("{:?}", mmu.cartridge.cartridge_type);
-    println!("{:?}", mmu.cartridge.cartridge_type.as_str());
-    println!("{:?}", mmu.cartridge.new_licensee_code);
-    println!("{:?}", mmu.cartridge.old_licensee_code);
-    println!("{:?}", mmu.cartridge.mask_rom_version_number);
-    println!("{:?}", mmu.cartridge.header_checksum);
-    println!("{:?}", mmu.cartridge.rom_banks_amount);
+    println!("{}", cpu.mmu.cartridge.title_to_string());
+    println!("{}", cpu.mmu.cartridge.rom_to_string());
+    println!("{}", cpu.mmu.cartridge.ram_to_string());
+    println!("{:?}", cpu.mmu.cartridge.destination_code);
+    println!("{:?}", cpu.mmu.cartridge.cartridge_type);
+    println!("{:?}", cpu.mmu.cartridge.cartridge_type.as_str());
+    println!("{:?}", cpu.mmu.cartridge.new_licensee_code);
+    println!("{:?}", cpu.mmu.cartridge.old_licensee_code);
+    println!("{:?}", cpu.mmu.cartridge.mask_rom_version_number);
+    println!("{:?}", cpu.mmu.cartridge.header_checksum);
+    println!("{:?}", cpu.mmu.cartridge.rom_banks_amount);
 
 }
