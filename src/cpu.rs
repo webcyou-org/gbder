831511538a73baf597f32dbeae306bb6a9b8dc2a
panicdragon <d.takayama.web@gmail.com>
2022-07-14 01:56:27 +0900

    feat: 🎸 create flag fnction

--- src/cpu.rs
+++ src/cpu.rs
@@ -71,13 +71,53 @@ impl CPU {
         self.l = (val & 0xff) as u8;
     }
 
+    // Z flag
+    fn set_f_z(&mut self, z: bool) {
+        self.f = (self.f & !(1 << 7)) | (u8::from(z) << 7);
+    }
+
+    fn f_z(&self) -> bool {
+        (self.f >> 7) & 1 == 1
+    }
+
+    // N flag
+    fn set_f_n(&mut self, n: bool) {
+        self.f = (self.f & !(1 << 6)) | (u8::from(n) << 6);
+    }
+
+    fn f_n(&self) -> bool {
+        (self.f >> 6) & 1 == 1
+    }
+
+    // H flag
+    fn set_f_h(&mut self, h: bool) {
+        self.f = (self.f & !(1 << 5)) | (u8::from(h) << 5);
+    }
+
+    fn f_h(&self) -> bool {
+        (self.f >> 5) & 1 == 1
+    }
+
+    // C flag
+    fn set_f_c(&mut self, c: bool) {
+        self.f = (self.f & !(1 << 4)) | (u8::from(c) << 4);
+    }
+
+    fn f_c(&self) -> bool {
+        (self.f >> 4) & 1 == 1
+    }
+
     pub fn step(&mut self) -> u8 {
         self.mmu.update(0);
         0
     }
 
     pub fn debug(&mut self) {
-        self.set_af(0x10ff);
+        // self.set_af(0x10ff);
+        // self.set_f_z(true);
+        // self.set_f_n(true);
+        self.set_f_c(true); 
+
         println!("af: {:?}", self.af());
         println!("a: {:?}", self.a);
         println!("f: {:?}", self.f);
@@ -89,6 +129,11 @@ impl CPU {
         println!("l: {:?}", self.l);
         println!("pc: {:?}", self.pc);
         println!("sp: {:?}", self.sp);
+
+        println!("flag z: {:?}", self.f_z());
+        println!("flag n: {:?}", self.f_n());
+        println!("flag c: {:?}", self.f_c());
+        println!("flag h: {:?}", self.f_h());
 
         let tick = self.step();
         println!("tick: {}", tick);
